54d994c028d608a454af128929742bbebeec5383
Cailey Cline <36306042+caileycline@users.noreply.github.com>
2018-05-10 14:13:35 -0500

    Cleaned up simple.py

--- scripting_examples/simple.py
+++ scripting_examples/simple.py
@@ -1,6 +1,7 @@
-# Simple demonstration of running an OpenRocket simulation from a python script
+# Simple demonstration of running an OpenRocket simulation from a Python script
 # Prints the event/time pairs from the simulation
-# Tested with jpype 0.5.4.2 and java 6.0
+# Tested with jpype 0.5.4.2 and Java 8.0
+# Note: this script should be run from the same folder as the .ork and openmeta-OpenRocket.jar
 
 import sys, traceback
 import numpy as np
@@ -8,21 +9,15 @@ import orhelper
 import time
 from jpype import *
 
-orhelper.OpenRocketInstance('..\OpenRocket.jar')
+# instantiate OpenRocket
+orhelper.OpenRocketInstance('OpenRocket.jar')
+# create instance of OpenRocket Helper class
 orh = orhelper.Helper()
 
 # Load document, run simulation and get events
+doc = orh.load_doc('simple.ork') # replace this with your .ork
+sim = doc.getSimulation(1)
+events = orh.get_events(sim)
 
-doc1 = orh.load_doc('..\ork_files\simple.ork')
-sim1 = doc1.getSimulation(1)
-events1 = orh.get_events(sim1)
-
-print "\nEvents #1"
-print events1
-
-doc2 = orh.load_doc('..\ork_files\\rollStabilizedRocket.ork')
-sim2 = doc2.getSimulation(0)
-events2 = orh.get_events(sim2)
-
-print "\nEvents #2"
-print events2
+print "\nSimulation Events"
+print events
